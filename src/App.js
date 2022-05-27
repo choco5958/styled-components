668fa4ced6f35800bad1abc7a83e1f43de7eaeb7
Han Dae Hyeon <choco5958@naver.com>
2022-05-27 19:20:10 +0900

    220527
    220527

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { css, ThemeProvider } from "styled-components";
+import Button from "./components/Button";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color || "black"};
+  border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Circle />
+      <Circle color="blue" />
+      <Circle color="red" huge />
+      {/* <AppBlock>
+        <Button>BUTTON</Button>
+      </AppBlock> */}
+      <ThemeProvider
+        theme={{
+          palette: {
+            blue: "#228be6",
+            gray: "#495057",
+            pink: "#f06595",
+          },
+        }}
+      >
+        <AppBlock>
+          <Button size="small">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="large">BUTTON</Button>
+
+          <Button color="gray" size="small">
+            BUTTON
+          </Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="large">
+            BUTTON
+          </Button>
+
+          <Button color="pink" size="small">
+            BUTTON
+          </Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="large">
+            BUTTON
+          </Button>
+        </AppBlock>
+      </ThemeProvider>
+    </>
   );
 }
 
